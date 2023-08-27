bd68abc3996805ce73545297024f696bdbdf4da9
Riddhi Dalal <124533399+riddle-me@users.noreply.github.com>
2023-08-27 13:13:01 +0530

    Create odd_even.py
    Write a program that takes an integer input from the user and determines whether it's odd or even.

---
+++ odd_even.py
@@ -0,0 +1,9 @@
+#Beginner Track 
+#Write a program that takes an integer input from the user and determines whether it's odd or even.
+
+a = int(input())
+if a % 2 == 0:
+  print("even")
+else:
+  print("odd")
+
