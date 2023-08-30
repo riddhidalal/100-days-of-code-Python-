38997a5fae4ac425aba00ce206f411be264da626
Riddhi Dalal <124533399+riddle-me@users.noreply.github.com>
2023-08-30 12:02:43 +0530

    Update Day_1_odd_even.py

--- Day_1_odd_even.py
+++ Day_1_odd_even.py
@@ -7,3 +7,13 @@ if a % 2 == 0:
 else:
   print("odd")
 
+'''
+Time Complexity : O(1)
+Time Complexity is calculated based upon the number of steps , operations and iterations in the program . 
+Number of operations = 1
+Number of iterations = 0
+Total Number of steps = 2 (operations + conditional check)
+Therefore O(2) --> O(1) which indicates that the program takes constant time for any input and is not dependent on the size of the input.
+
+Space Complexity = O(1) which indicated that the program takes constant space and does not change with the input . 
+
