80c6b9a3d2c9e6f3beb57a929c2f184714c48cd1
Riddhi Dalal <124533399+riddle-me@users.noreply.github.com>
2023-08-30 11:40:12 +0530

    Create Day_4_palindrome.py
    Write a program that checks whether a given string is a palindrome or not. 
    Time Complexity
    Space Complexity

---
+++ Day_4_palindrome.py
@@ -0,0 +1,33 @@
+#palindrome _check
+#Write a program that checks whether a given string is a palindrome or not.
+
+string = str(input("Please enter the word : "))
+string_rev = string[-1::-1]   #slicing
+
+if string == string_rev:
+  print("Yes! Its a palindrome!")
+else:
+  print("No, this is not a palindrome!")
+  print("Try with a different word")
+
+'''
+Time Complexity:
+Hi! Let's calculate the time complexity of the code.
+Let's go stepwise:
+1. Reading input :
+Reading input takes O(n) time where n is the length of the string . 
+2. String reversal :
+When we reverse the string we are traversing from 1st letter of the string to the last letter .
+Hence , this also takes O(n) time complexity.
+3. String comparison :
+In string comparison , we traverse from first string to last . It also takes O(n) complexity .
+
+Hence T = n+n+n = 3n
+But since constants can be neglected , Time complexity becomes O(n).
+
+Space complexity - O(1) i.e constant size is used by the program .
+
+
+
+
+
